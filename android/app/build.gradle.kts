d942cdbc93b9350edb9401e97249fe497df18bb4
sulakkana <sulakkanasewwandi02@gmail.com>
2025-06-23 08:44:33 +0530

    add dependency

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -12,6 +12,7 @@ android {
     namespace = "com.example.smarteggtray"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
+     ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -22,16 +23,14 @@ android {
         jvmTarget = JavaVersion.VERSION_11.toString()
     }
 
-    defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.smarteggtray"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode
-        versionName = flutter.versionName
-    }
+ defaultConfig {
+    applicationId = "com.example.smarteggtray"
+    minSdk = 23  // ✅ Update this directly here
+    targetSdk = flutter.targetSdkVersion
+    versionCode = flutter.versionCode
+    versionName = flutter.versionName
+}
+
 
     buildTypes {
         release {
